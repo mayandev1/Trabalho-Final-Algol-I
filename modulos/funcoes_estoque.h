aa70c0a187eb9c0b71508a4f609c32bed64ef856
mayan.dev <mayangabriel654@gmail.com>
2025-06-02 18:02:46 -0300

    atualizações

---
+++ modulos/funcoes_estoque.h
@@ -0,0 +1,12 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+void cadastrarItem();
+void excluirItem();
+void editarItem();
+void buscarItem();
+void listarItem();
+void limparBuffer();
+void limparTela();
+
+#endif
