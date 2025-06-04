f9931c45eac36cfbbc6c31511b5eda737eb1003f
Lyan  fernando <lyanfernando7@gmail.com>
2025-06-04 20:16:35 -0300

    Update trabalho_principal.c

--- trabalho_principal.c
+++ trabalho_principal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
- #define MAX 1
+ #define MAX 100
 
     int cadastrarproduto(int codigo[],char nome[][50],float preco[],int quantidade){
         if(quantidade >= MAX){
